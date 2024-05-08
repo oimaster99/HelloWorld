89df64f4cbd6db27d5e9e559054334a3153ba508
oimaster99 <oimaster99@gmail.com>
2024-05-08 12:03:32 -0600

    added triangle function

--- shapes.py
+++ shapes.py
@@ -4,5 +4,11 @@
 
 def main():
     print("this is our shapes program")
+    tri_area = calc_tri_area(5, 12)
+    print(f"The area of triangle is {tri_area}.")
+
+def calc_tri_area(b, h):
+    area = 1/2 * h * b
+    return area
 
 main()
